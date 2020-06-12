d7c6c9b4898d6cab87741589ed50a2c38fedf14a
James Canning <jc78@geneseo.edu>
2020-06-12 19:06:04 -0400

    adding some more comments

--- pythonFiles/draw_knots.py
+++ pythonFiles/draw_knots.py
@@ -1,12 +1,21 @@
+#!opt/anaconda3/bin/python
 """
 This file uses functions from mosaic_drawing.py to draw knots from a given csv file, or individual knots from a given
 matrix
+
+IF YOU ARE DRAWING KNOTS FROM A CSV FILE:
+Comment out the section for drawing an individual knot at the end of the script
 We assume the csv is of a specific format, with columns: (OtherName), Name, Vector, Crossings, Mosaic, TileNum
   - That is, the first column is not used, the second is the name of the knots, the third is the vector that filled in
   the layout for the knot, the fourth is the number of crossings in the mosaic, the fifth is the size of the mosaic, and
   the sixth is the number of non-blank tiles used
 
 Make sure to set table_path to your own path to the csv
+
+IF YOU ARE DRAWING ONE KNOT:
+Comment out the section for drawing from a csv at the beginning of the script
+Specify the mosaic size, how large to scale the image, the layout, the vector to fill in the layout, and the save path
+
 James Canning, May 2020
 """
 
@@ -119,7 +128,7 @@ layoutDict = {"4-12": m4_12, "5-17": m5_17, "6-22": m6_22, "6-24": m6_24, "6-27"
 ''' For drawing knots from a csv file '''
 # ****************************************
 
-
+"""
 # path to csv file
 table_path = "C://Users//Owner//Documents//Geneseo//Heap Research//files//7_1-sorted-trimmed.csv"
 df = pd.read_csv(table_path)
@@ -157,13 +166,14 @@ for i in range(df.shape[0]):
     draw_knot(mTemp, surface, tile_scale, mosaic_size)
     # If creating png files, uncomment the next line (this can be done in addition to the eps files)
     #surface.write_to_png(save_path + str(name) + ".png")
+"""
+
 
 # ****************************************
-''' For drawing knots from a given vector'''
+''' For drawing knots from a given set of vectors'''
 # ****************************************
 
-'''
-# Specify size of masaic and how large to scale the image
+# Specify size of mosaic and how large to scale the image
 mosaic_size = 7
 tile_scale = 50
 # Choose your layout
@@ -172,14 +182,14 @@ m = layoutDict['7-31'].copy()
 v = [10,7,7,10,7,8,7,10,7,9,10,8,7,9,9,10,9,10]
 m[m == -1] = v
 
-save_path = "C://Users//Owner//Documents//Geneseo//Heap Research//images//knots//10_20-34tiles.eps"
-# Use the following for making eps files
-surface = cairo.PSSurface(save_path, mosaic_size * tile_scale, mosaic_size * tile_scale)
+save_path = "./"
+# Use the following line for making eps files
+#surface = cairo.PSSurface(save_path, mosaic_size * tile_scale, mosaic_size * tile_scale)
 
-# Use the following for making png files
-#surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, mosaic_size * tile_scale, mosaic_size * tile_scale)
+# Use the following line for making png files
+surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, mosaic_size * tile_scale, mosaic_size * tile_scale)
 
 draw_knot(m, surface, tile_scale, mosaic_size)
-# If creating a png file, uncomment the next line   
-#surface.write_to_png(save_path + str(name) + ".png")
-'''
+# If creating a png file, uncomment the next line
+surface.write_to_png(save_path + str(name) + ".png")
+
